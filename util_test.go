f6ffdf93000b1e080e06e06ebe8822bd5fa9c639
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-03-13 21:56:37 +0900

    fix time parse

--- util_test.go
+++ util_test.go
@@ -6,9 +6,15 @@ import (
 )
 
 func TestTimep(t *testing.T) {
-	want := "2023-02-03T18:19:20.333Z"
+	want := "2023-02-03T18:19:20Z"
 	got := timep(want).UTC().Format(time.RFC3339)
 	if got != want {
+		t.Fatalf("want %q but got %q", want, got)
+	}
+
+	want = "2023-02-03T18:19:20.333Z"
+	got = timep(want).UTC().Format(time.RFC3339)
+	if got == want {
 		t.Fatalf("want %q but got %q", want, got)
 	}
 
