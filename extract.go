430d9c38206f164b29d7692d150dc81b05ecf65d
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-12-19 16:59:35 +0900

    extract tags

--- extract.go
+++ extract.go
@@ -8,11 +8,13 @@ import (
 const (
 	urlPattern     = `https?://[-A-Za-z0-9+&@#\/%?=~_|!:,.;\(\)]+`
 	mentionPattern = `@[a-zA-Z0-9.]+`
+	tagPattern     = `\b#\S+\B`
 )
 
 var (
 	urlRe     = regexp.MustCompile(urlPattern)
 	mentionRe = regexp.MustCompile(mentionPattern)
+	tagRe     = regexp.MustCompile(tagPattern)
 )
 
 type entry struct {
@@ -66,6 +68,32 @@ func extractMentionsBytes(text string) []entry {
 	for _, m := range matches {
 		result = append(result, entry{
 			text:  strings.TrimPrefix(text[m[0]:m[1]], "@"),
+			start: int64(len(text[0:m[0]])),
+			end:   int64(len(text[0:m[1]]))},
+		)
+	}
+	return result
+}
+
+func extractTags(text string) []entry {
+	var result []entry
+	matches := tagRe.FindAllStringSubmatchIndex(text, -1)
+	for _, m := range matches {
+		result = append(result, entry{
+			text:  text[m[0]:m[1]],
+			start: int64(len([]rune(text[0:m[0]]))),
+			end:   int64(len([]rune(text[0:m[1]])))},
+		)
+	}
+	return result
+}
+
+func extractTagsBytes(text string) []entry {
+	var result []entry
+	matches := tagRe.FindAllStringSubmatchIndex(text, -1)
+	for _, m := range matches {
+		result = append(result, entry{
+			text:  text[m[0]:m[1]],
 			start: int64(len(text[0:m[0]])),
 			end:   int64(len(text[0:m[1]]))},
 		)
